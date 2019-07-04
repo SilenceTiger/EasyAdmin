e9368248714fbd3ea454a9d8cbe08a300b192da6
silencetiger <547989722@qq.com>
2019-07-04 18:54:23 +0800

    some change in echarts

--- src/views/echarts/BaseChart.vue
+++ src/views/echarts/BaseChart.vue
@@ -1,6 +1,7 @@
 <template>
-   <div class="chart-container">
+  <div class="chart-container">
     <div class="chart-wrap" id="bar-chart"></div>
+    <div class="chart-wrap" id="pie-chart"></div>
   </div>
 </template>
 
@@ -12,9 +13,10 @@ export default {
     return {};
   },
   mounted() {
-    var myChart = echarts.init(document.getElementById("bar-chart"));
+    var that = this;
+    var barChart = echarts.init(document.getElementById("bar-chart"));
     // 绘制图表
-    myChart.setOption({
+    barChart.setOption({
       title: {
         text: "ECharts 入门示例"
       },
@@ -31,6 +33,64 @@ export default {
         }
       ]
     });
+
+    var pieChart = echarts.init(document.getElementById("pie-chart"));
+    // 绘制图表
+    pieChart.setOption({
+      title: {
+        text: "某站点用户访问来源",
+        subtext: "纯属虚构",
+        x: "center"
+      },
+      // tooltip: {
+      //   trigger: "item",
+      //   formatter: "{a} <br/>{b} : {c} ({d}%)"
+      // },
+      // legend: {
+      //   orient: "vertical",
+      //   left: "left",
+      //   data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"]
+      // },
+      label: {
+        formatter: function(val) {
+          // console.log(val);
+          return val.name + ":" + val.percent + " %" + "\n" + "xxxxx"
+        }
+      },
+      series: [
+        {
+          name: "访问来源",
+          type: "pie",
+          radius: "55%",
+          center: ["50%", "60%"],
+          data: [
+            { value: 335, name: "直接访问" },
+            { value: 310, name: "邮件营销" },
+            { value: 234, name: "联盟广告" },
+            { value: 135, name: "视频广告" },
+            { value: 1548, name: "搜索引擎" }
+          ],
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)"
+            }
+          }
+        }
+      ]
+    });
+
+    pieChart.on("click", function(params) {
+      // 控制台打印数据的名称
+      that.pieChartClickEvent(params);
+    });
+  },
+
+  methods: {
+    pieChartClickEvent(params) {
+      console.log(params);
+    }
   }
 };
 </script>
